d1954dd6c9b83ab2ab46a6ab56164c4c7bd9ec6e
Pablo Caballero <deidigitalis@gmail.com>
2022-10-10 20:22:33 +0200

    test: Add grades.

--- IntegrationTests/GradeDbContextTests.Create.cs
+++ IntegrationTests/GradeDbContextTests.Create.cs
@@ -1,10 +1,26 @@
 ﻿namespace IntegrationTests
 {
     using FluentAssertions;
+    using GradeApi.Persistence.Entitites;
     using Xunit;
 
     public partial class GradeDbContextTests
     {
+        [Fact]
+        public void Add_Grade()
+        {
+            var miGrado = new Grade();
+            miGrado.Name = "Nombre del grado";
+            miGrado.Section = "Sección de ejemplo";
+
+            _dbContext.Grades.Add(miGrado);
+
+            int noCambios = _dbContext.SaveChanges();
+
+            noCambios.Should().Be(1);
+            miGrado.Id.Should().NotBe(0);
+        }
+
         /// <summary>
         /// Añadir un estudiante
         /// </summary>
