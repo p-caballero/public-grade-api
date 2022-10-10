f0b5b9d0cbc26638e3127f4919ab852e695b14f6
Pablo Caballero <deidigitalis@gmail.com>
2022-10-10 19:28:38 +0200

    test: Solutions.

--- IntegrationTests/GradeDbContextTests.Read.cs
+++ IntegrationTests/GradeDbContextTests.Read.cs
@@ -3,6 +3,8 @@
     using FluentAssertions;
     using Microsoft.EntityFrameworkCore;
     using System.Linq;
+    using System.Text.Json;
+    using System.Threading.Tasks;
     using Xunit;
 
     public partial class GradeDbContextTests
@@ -28,8 +30,24 @@
         [Fact]
         public void Get_all_students_with_courses()
         {
-            // TODO: Obtener todos los estudiantes con sus cursos
-            false.Should().BeTrue("Pendiente");
+            // Act
+            var actual = _dbContext.Students
+                .Include(x => x.StudentCourses)
+                .ToList();
+
+            // Assert
+            actual.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public void Get_all_courses_with_students()
+        {
+            var allCourses = _dbContext.StudentCourses
+                .Include(x => x.Course)
+                .Select(x => x.Course)
+                .Distinct()
+                .OrderBy(x => x.Name)
+                .ToList();
         }
 
         /// <summary>
@@ -38,9 +56,59 @@
         [Fact]
         public void Get_all_student_names()
         {
-            // TODO: Obtener todos los nombres de los estudiantes
-            false.Should().BeTrue("Pendiente");
+            var allNames = _dbContext.Students
+                .Select(x => x.Name)
+                .ToList();
         }
+
+        [Fact]
+        public void Get_all_student_with_name_diego()
+        {
+            var allNames = _dbContext.Students
+                .Where(x => x.Name.Contains("Diego"))
+                .Select(x => x.Name)
+                .ToList();
+        }
+
+        private class EstudianteNombreYPeso
+        {
+            public string Nombre { get; set; }
+
+            public int Peso { get; set; }
+        }
+
+        [Fact]
+        public void Get_all_student_weight_greater_90()
+        {
+            var result = _dbContext.Students
+                .Where(x => x.Weight != null && x.Weight > 90)
+                .Select(x => new { Nombre = x.Name, Peso = x.Weight }) // Proyección
+                .OrderByDescending(x => x.Nombre)
+                .First();
+
+            var entDominio = new EstudianteNombreYPeso
+            {
+                Nombre = result.Nombre,
+                Peso = (int)result.Peso.Value
+            };
+        }
+
+        [Fact]
+        public async Task Get_all_student_weight_greater_90_async()
+        {
+            var result = await _dbContext.Students
+                .Where(x => x.Weight != null && x.Weight > 90)
+                .Select(x => new { Nombre = x.Name, Peso = x.Weight }) // Proyección
+                .OrderByDescending(x => x.Nombre)
+                .FirstAsync();
+
+            var entDominio = new EstudianteNombreYPeso
+            {
+                Nombre = result.Nombre,
+                Peso = (int)result.Peso.Value
+            };
+        }
+
 
         /// <summary>
         /// Todos los créditos de un estudiante
