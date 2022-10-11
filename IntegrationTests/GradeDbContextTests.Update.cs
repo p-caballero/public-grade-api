063a2ad555170dda0cd1bf065727ca7bcc1568ae
Pablo Caballero <deidigitalis@gmail.com>
2022-10-11 13:02:01 +0200

    test: Update tests.

--- IntegrationTests/GradeDbContextTests.Update.cs
+++ IntegrationTests/GradeDbContextTests.Update.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IntegrationTests
+﻿namespace IntegrationTests
 {
-	public partial class GradeDbContextTests
+    using FluentAssertions;
+    using Xunit;
+
+    public partial class GradeDbContextTests
 	{
+        /// <summary>
+        /// Modifica un estudiante con todo el nombre en mayúscula
+        /// </summary>
+        [Fact]
+        public void Update_student_name_uppercase()
+        {
+            // TODO: Modifica un estudiante con todo el nombre en mayúscula
+            false.Should().BeTrue("Pendiente");
+        }
+
+        /// <summary>
+        /// Todos los estudiantes del grado 1 suben 1 kilo si tienen el peso
+        /// </summary>
+        [Fact]
+        public void Update_student_weight_of_a_grade()
+        {
+            // TODO: Todos los estudiantes del grado 1 suben 1 kilo si tienen el peso
+            false.Should().BeTrue("Pendiente");
+        }
     }
 }
