e890302824fa384e7554eb65acb745e066102dbb
Pablo Caballero <deidigitalis@gmail.com>
2022-10-11 21:42:50 +0200

    fix: Removed unneeded relation.

--- GradeApi/Persistence/Entitites/Grade.cs
+++ GradeApi/Persistence/Entitites/Grade.cs
@@ -12,11 +12,8 @@
 
         public Grade()
         {
-            Students = new HashSet<Student>();
             Courses = new HashSet<Course>();
         }
-
-        public virtual ICollection<Student> Students { get; set; }
 
         public virtual ICollection<Course> Courses { get; set; }
     }
